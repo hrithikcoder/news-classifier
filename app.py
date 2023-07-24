7c3f397463f45c26cf8e29a8ef616d2c63485eea
hrithikcoder <108560837+hrithikcoder@users.noreply.github.com>
2023-07-24 12:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 
-model=pickle.load(open("fakenews_model.pkl",'rb'))
-vector=pickle.load(open("vectorizer.pkl",'rb'))
-
-
-
-
+model = pickle.load(open("fakenews_model.pkl", 'rb'))
+vectorizer = pickle.load(open("vectorizer.pkl", 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello World"
+    return "Hello, World!"
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    news=str(request.form['news'])
-    result=model.predict(vector.transform([news]))[0]
+    news = str(request.form['news'])
+    vectorized_news = vectorizer.transform([news])
+    result = model.predict(vectorized_news)[0]
     return jsonify(result)
 
 if __name__ == '__main__':
-
-    app.run()
+    app.run(debug=True, port=8000)
